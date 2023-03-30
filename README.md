6e954d774b343ca106b2e3742b5b5ae18299d07e
Konstantin Weddige <konstantin@skathi.net>
2023-03-30 12:35:32 +0200

    Add some documentation and bump version to 0.2

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # django-hibp
 
-This package provides a password validator for django that validates passwords against [HIBP](https://haveibeenpwned.com/Passwords). 
+This package provides a password validator for Django that validates passwords against [HIBP](https://haveibeenpwned.com/Passwords).
+
+## Getting started
+
+Install using pip:
+
+```
+pip install django-hibp
+```
+
+Add `HIBPPasswordValidator` to your `AUTH_PASSWORD_VALIDATORS` setting:
+
+```python
+AUTH_PASSWORD_VALIDATORS = [
+    ...
+    {
+        'NAME': 'django_hibp.HIBPPasswordValidator',
+        'OPTIONS': {
+            'fail_on_error': False,
+        }
+    },
+]
+```
+
+Since this plugin relies on an external API, this introduces a new point of failure. If the Pwned Passwords API is inaccessible, the check will fail. To avoid breaking your application in this case, set `fail_on_error` to `False`.
--- django_hibp.py
+++ django_hibp.py
@@ -1,33 +1,34 @@
 import hashlib
+import logging
+import urllib.error
 import urllib.request
 
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext as _
+
+logger = logging.getLogger(__name__)
 
 
 class HIBPPasswordValidator:
     _API = "https://api.pwnedpasswords.com/range/{0}"
 
     def __init__(self, fail_on_error=True):
-        self._fail_on_error = fail_on_error
+        self.fail_on_error = fail_on_error
 
     def validate(self, password, user=None):
         hash = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
         try:
-            response = (
-                urllib.request.urlopen(self._API.format(hash[:5]))
-                .read()
-                .decode("utf-8")
-            )
-        except:
+            response = urllib.request.urlopen(self._API.format(hash[:5])).read().decode("utf-8")
+        except urllib.error.HTTPError as e:
             # As this validator calls the HIBP API, network problems or HIBP being offline can cause errors.
-            if self._fail_on_error:
+            if self.fail_on_error:
+                logger.error(f"Password validation failed: {e}")
                 raise ValidationError(
-                    _(
-                        "Failed to validate if the password has previously appeared in a data breach."
-                    ),
+                    _("Failed to validate if the password has previously appeared in a data breach."),
                     code="password_has_been_pwned",
                 )
+            else:
+                logger.warning(f"Password validation failed: {e}")
         if hash[5:] in response:
             raise ValidationError(
                 _("This password has previously appeared in a data breach."),
